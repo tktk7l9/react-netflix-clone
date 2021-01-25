4aa86c95c018543e19e3d6cad438d774c58715c2
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-25 23:25:37 +0900

    映画の画像クリックでトレイラーを表示する

--- src/compornents/Row.tsx
+++ src/compornents/Row.tsx
@@ -20,8 +20,17 @@ type Movie = {
   backdrop_path: string;
 };
 
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
   //urlが更新される度に
   useEffect(() => {
@@ -33,7 +42,25 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  const opts: Options = {
+    height: "390",
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerUrl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=332252283d94446982784f4aa658d2a5`
+      );
+      setTrailerUrl(trailerUrl.data.results[0]?.key);
+    }
+  };
 
   return (
     <div className="Row">
@@ -48,9 +75,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
